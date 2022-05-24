cd7336ffe70fd340c5680b4e87d7fc803300a258
Lucas Huguenin <l-huguenin@hotmail.com>
2022-05-24 20:24:52 -0300

    Firebase initial config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAjYUW4YI90Fp4xeDQa-mMvy05RdnuGnsw",
+  authDomain: "personal-website-13870.firebaseapp.com",
+  projectId: "personal-website-13870",
+  storageBucket: "personal-website-13870.appspot.com",
+  messagingSenderId: "131401058975",
+  appId: "1:131401058975:web:b4a5ac9d773e40c0c530aa",
+  measurementId: "G-1QBH3RM209"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <section>
+        <Home />
+      </section>
     </div>
   );
+}
+
+function Home() {
+  return (
+    <button>PRESS ME</button>
+  )
 }
 
 export default App;
